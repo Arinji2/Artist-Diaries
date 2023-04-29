6fa729e13c3c8707804e00b60591712bdbb55c9e
Arinji2 <arinjaydhar205@gmail.com>
2023-04-29 22:48:11 +0530

    Reformat #3 (Topics)

--- pages/topics/charcoal.tsx
+++ pages/topics/charcoal.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
 import { useEffect, useRef, useState } from "react";
 import { useIntersection } from "react-use";
 import type { NextPage } from "next";
--- pages/topics/realistic.tsx
+++ pages/topics/realistic.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useRef, useState } from "react";
 import { useIntersection } from "react-use";
 import type { NextPage } from "next";
